da7e3aeb7742a1bd1e8a5fb0b6b9d56f80e0b775
Explosy <moroza.exe@yandex.ru>
2022-09-08 16:01:49 +0300

    add random choose the sort

--- Program.cs
+++ Program.cs
@@ -8,7 +8,13 @@ namespace RandomList
         {
             Console.WriteLine("Добро пожаловать в программу RandomListGenerator");
             RandomListGenerator randomListGenerator = new RandomListGenerator();
-
+            var newRandomList = randomListGenerator.GetRandomList(20);
+            ListSorter listSorter = new ListSorter();
+            listSorter.SortList(newRandomList);
+            foreach(var item in newRandomList)
+            {
+                Console.Write(item + " ");
+            }
         }
     }
 }
---
+++ SortMethods/InsertSort.cs
@@ -0,0 +1,26 @@
+﻿namespace RandomList
+{
+    /// <summary>
+    /// Класс реализующий сортировку массива методом вставки
+    /// </summary>
+    internal class InsertSort : ISort
+    {
+        public void Sort(int[] array)
+        {
+            int temp;
+            int index;
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp = array[i];
+                index = i;
+
+                while (index > 0 && temp < array[index - 1])
+                {
+                    array[index] = array[index - 1];
+                    index--;
+                }
+                array[index] = temp;
+            }
+        }
+    }
+}
