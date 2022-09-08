c966595260f6932c192eea4295f56f105650ddf7
Explosy <moroza.exe@yandex.ru>
2022-09-08 13:49:22 +0300

    Add Bubble Sort and Insert Sort to ListSorter

--- ListSorter.cs
+++ ListSorter.cs
@@ -6,7 +6,59 @@ using System.Threading.Tasks;
 
 namespace RandomList
 {
-    internal class ListSorter
+    public class ListSorter
     {
+        /// <summary>
+        /// Функция сортировки массива. Алгоритм сортировки выбирается случайным образом из: "","",""
+        /// </summary>
+        /// <param name="unsortedList">Неотсортированный массив System.Int32[]</param>
+        /// <returns>Сортированный массив System.Int32[]</returns>
+        public void SortList(int[] unsortedList)
+        {
+            BubbleSort(unsortedList);
+        }
+        /// <summary>
+        /// Сортировка массива пузырьком
+        /// </summary>
+        /// <param name="array">Неотсортированный массив</param>
+        /// <returns></returns>
+        private void BubbleSort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - i - 1; j++)
+                {
+                    if (array[j] > array[j+1])
+                    {
+                        temp = array[j];
+                        array[j] = array[j+1];
+                        array[j+1] = temp;
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Сортировка массива методом вставки
+        /// </summary>
+        /// <param name="array">Неотсортированный массив</param>
+        private void InsertSort(int[] array)
+        {
+            int temp;
+            int index;
+            for (int i = 0; i < array.Length; i++)
+            {
+                temp = array[i];
+                index = i;
+
+                while (index > 0 && temp < array[index-1])
+                {
+                    array[index] = array[index - 1];
+                    index--;
+                }
+                array[index] = temp;
+            }
+        }
+
     }
 }
